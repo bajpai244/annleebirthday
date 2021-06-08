992222348a5e80a5978d22ba0e2b9fe5ba8fb3d2
bajpai244 <bajpaiharsh244@gmail.com>
2021-06-09 01:29:52 +0530

    feat: add support for bg music

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+
 import Hero from "../components/hero";
 import Gallery from "../components/gallery";
 
+import useSound from "use-sound";
+
 const Page = () => {
+  const [play] = useSound("./music.mp3");
+
+  useEffect(() => {
+    play();
+  });
+
   return (
     <>
       <Hero />
